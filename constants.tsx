de8d979c63a703caf01fa20dfffc01bb130ba40e
matician255 <mathematiciangodfrey@outlook.com>
2026-02-19 01:52:13 +0300

    editing again

--- constants.tsx
+++ constants.tsx
@@ -425,8 +425,8 @@ export const PROJECTS: Project[] = [
     body {
       font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
       line-height: 1.6;
-      color: #e9eef6;
-      background-color:rgb(17, 16, 16);
+      color: #000;
+      background-color: #fff;
       margin: 0;
       padding: 0;
     }
@@ -607,7 +607,7 @@ export const PROJECTS: Project[] = [
       text-align: center;
       padding: 24px;
       font-size: 0.83rem;
-      color: #bdbdbd;
+      color: #6c757d;
     }
   </style>
 </head>
